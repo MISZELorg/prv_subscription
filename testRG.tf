56bd4f959133a83696ff4553c0ed123b3e9c9fa9
kmiszel <kmiszel@sii.pl>
2023-03-22 14:47:22 +0100

    checkov

--- testRG.tf
+++ testRG.tf
@@ -8,17 +8,17 @@ resource "azurerm_resource_group" "testRG456" {
   location = var.location
 }
 
-# resource "azurerm_resource_group" "testRG3" {
-#   name     = "testRG3"
-#   location = var.location
-# }
+resource "azurerm_resource_group" "testRG3" {
+  name     = "testRG3"
+  location = var.location
+}
 
-# resource "azurerm_resource_group" "testRG4" {
-#   name     = "testRG4"
-#   location = var.location
-# }
+resource "azurerm_resource_group" "testRG4" {
+  name     = "testRG4"
+  location = var.location
+}
 
-# resource "azurerm_resource_group" "testRG5" {
-#   name     = "testRG5"
-#   location = var.location
-# }
+resource "azurerm_resource_group" "testRG5" {
+  name     = "testRG5"
+  location = var.location
+}
