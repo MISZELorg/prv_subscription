1c0ae6832a13180c2a5d5ce6b7e7e8c5c5f4c1f6
kmiszel <kmiszel@sii.pl>
2023-03-22 15:47:22 +0100

    234234

--- main.tf
+++ main.tf
@@ -1,15 +1,19 @@
 terraform {
-	required_providers {
-		azurerm = {
-	    	source = "hashicorp/azurerm"
-	    	version = "~>3.40.0"
-	    }
+  required_providers {
+	azurerm = {
+	  source = "hashicorp/azurerm"
+	  version = "~>3.40.0"
 	}
+  }
 
-    backend "azurerm" {
-	  	resource_group_name = "prvbackend-RG"
-	  	storage_account_name = "prvbackend"
-	  	container_name = "tfstate"
-	  	key = "tfstate"
-    }
+  backend "azurerm" {
+	resource_group_name = "prvbackend-RG"
+	storage_account_name = "prvbackend"
+	container_name = "tfstate"
+	key = "tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
