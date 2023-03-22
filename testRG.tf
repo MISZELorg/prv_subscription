bb3d2e483df1b9835e29ec3310ff6665b52f3d82
kmiszel <kmiszel@sii.pl>
2023-03-22 11:10:59 +0100

    first files

---
+++ testRG.tf
@@ -0,0 +1,24 @@
+resource "azurerm_resource_group" "testRG123" {
+  name     = "testRG123"
+  location = var.location
+}
+
+# resource "azurerm_resource_group" "testRG2" {
+#   name     = "testRG2"
+#   location = var.location
+# }
+
+# resource "azurerm_resource_group" "testRG3" {
+#   name     = "testRG3"
+#   location = var.location
+# }
+
+# resource "azurerm_resource_group" "testRG4" {
+#   name     = "testRG4"
+#   location = var.location
+# }
+
+# resource "azurerm_resource_group" "testRG5" {
+#   name     = "testRG5"
+#   location = var.location
+# }
