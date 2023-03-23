a7c1b86ec6ed786af3fc09b246dd9a7def66c464
kmiszel <kmiszel@sii.pl>
2023-03-23 10:05:03 +0100

    SA no tests

--- SA.tf
+++ SA.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "testsa" {
   name                     = "kmiszeltestsa"
-  resource_group_name      = azurerm_resource_group.testRG123
+  resource_group_name      = "testRG123"
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
